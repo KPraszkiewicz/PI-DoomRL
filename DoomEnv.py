4722eb9cb84e3bc800b43ef66b88c81becd2ad82
KP <konradiusIII@gmail.com>
2023-12-16 17:57:39 +0100

    add less button combinations

--- DoomEnv.py
+++ DoomEnv.py
@@ -6,9 +6,20 @@ import gymnasium as gym
 from gymnasium import Env
 from gymnasium import spaces
 from stable_baselines3.common import vec_env
+from vizdoom import Button
+import itertools
 
 Frame = np.ndarray
 
+# Buttons that cannot be used together
+MUTUALLY_EXCLUSIVE_GROUPS = [
+    [Button.MOVE_RIGHT, Button.MOVE_LEFT],
+    [Button.TURN_RIGHT, Button.TURN_LEFT],
+    [Button.MOVE_FORWARD, Button.MOVE_BACKWARD],
+]
+
+# Buttons that can only be used alone.
+EXCLUSIVE_BUTTONS = [Button.ATTACK]
 
 class DoomEnv(Env):
     """Wrapper environment following OpenAI's gym interface for a VizDoom game instance."""
@@ -29,7 +40,9 @@ class DoomEnv(Env):
 
         # Assign other variables
         self.game = game
-        self.possible_actions = np.eye(self.action_space.n).tolist()  # VizDoom needs a list of buttons states.
+        # self.possible_actions = np.eye(self.action_space.n).tolist()  # VizDoom needs a list of buttons states.
+        self.possible_actions = get_available_actions(game.get_available_buttons())
+        print(self.possible_actions)
         self.frame_skip = frame_skip
         self.frame_processor = frame_processor
 
@@ -63,6 +76,8 @@ class DoomEnv(Env):
             The initial state of the new environment.
         """
         self.game.new_episode()
+        if seed:
+            self.game.set_seed(seed)
         self.state = self._get_frame()
 
         return self.state, {}
@@ -83,6 +98,7 @@ class DoomEnv(Env):
         else:
             return self.empty_frame
 
+# ENVS
 
 def create_env(scenario: str, visible = False, **kwargs) -> DoomEnv:
     # Create a VizDoom instance.
@@ -101,3 +117,36 @@ def create_vec_env(n_envs=1, **kwargs) -> vec_env.VecTransposeImage:
 
 def create_eval_vec_env(**kwargs) -> vec_env.VecTransposeImage:
     return create_vec_env(n_envs=1, **kwargs)
+
+# BUTTONS
+
+def has_exclusive_button(actions: np.ndarray, buttons: np.array) -> np.array:
+    exclusion_mask = np.isin(buttons, EXCLUSIVE_BUTTONS)
+    
+    # Flag actions that have more than 1 active button among exclusive list.
+    return (np.any(actions.astype(bool) & exclusion_mask, axis=-1)) & (np.sum(actions, axis=-1) > 1)
+
+def has_excluded_pair(actions: np.ndarray, buttons: np.array) -> np.array:
+    # Create mask of shape (n_mutual_exclusion_groups, n_available_buttons), marking location of excluded pairs.
+    mutual_exclusion_mask = np.array([np.isin(buttons, excluded_group) 
+                                      for excluded_group in MUTUALLY_EXCLUSIVE_GROUPS])
+
+    # Flag actions that have more than 1 button active in any of the mutual exclusion groups.
+    return np.any(np.sum(
+        # Resulting shape (n_actions, n_mutual_exclusion_groups, n_available_buttons)
+        (actions[:, np.newaxis, :] * mutual_exclusion_mask.astype(int)),
+        axis=-1) > 1, axis=-1)
+
+def get_available_actions(buttons: np.array) -> t.List[t.List[float]]:
+    # Create list of all possible actions of size (2^n_available_buttons x n_available_buttons)
+    action_combinations = np.array([list(seq) for seq in itertools.product([0., 1.], repeat=len(buttons))])
+
+    # Build action mask from action combinations and exclusion mask
+    illegal_mask = (has_excluded_pair(action_combinations, buttons)
+                    | has_exclusive_button(action_combinations, buttons))
+
+    possible_actions = action_combinations[~illegal_mask]
+    possible_actions = possible_actions[np.sum(possible_actions, axis=1) > 0]  # Remove no-op
+
+    print(f'Built action space of size {len(possible_actions)} from buttons {buttons}')
+    return possible_actions.tolist()
