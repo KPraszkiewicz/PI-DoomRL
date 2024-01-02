9be85e05618a513114ad395c1a7c7c001ed0adda
KP <konradiusIII@gmail.com>
2024-01-02 22:21:00 +0100

    update test

--- test2.py
+++ test2.py
@@ -1,26 +1,34 @@
 import cv2
+import time
 from stable_baselines3 import DQN, PPO
 import numpy as np
-import DoomEnv
+import DoomHealthGathering
 from utils import load_config
-
+from env_utils import *
+from DoomEnv import DoomEnv
+from DoomWithBots import DoomWithBots
 
 env_args = {
-    'scenario': "take_cover",
+    'scenario': "health_gathering",
     'visible': True,
+    'EnvClass': DoomEnv,
     'frame_skip': 1,
     'frame_processor': lambda frame: cv2.resize(
-        frame, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
+        frame, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA),
+    'combinated_buttons': False,
+    # 'n_bots': 8
 }
 
-env = DoomEnv.create_env(**env_args )
+env = create_env(**env_args )
+# env = env_with_bots(**env_args )
 
 
 obs = env.reset()
 for i in range(10000):
     action = env.action_space.sample()
     obs, reward, done, _, info = env.step(action)
-    
+    print(reward)
     # VecEnv resets automatically
-    # if done:
-    #   obs = vec_env.reset()
+    if done:
+      obs = env.reset()
+    time.sleep(100)
